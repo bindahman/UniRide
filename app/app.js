e621f6fb4900c784c59c63bf7d8f0d92bbd14747
Osman-Ebrahim <161317906+Osman-Ebrahim@users.noreply.github.com>
2025-03-18 11:41:08 +0000

    Update app.js
    edited code

--- app/app.js
+++ app/app.js
@@ -112,18 +112,4 @@ app.get("/db_test", function(req, res) {
 
 
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
 
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
-});
